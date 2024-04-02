15969370b3c364d240bbefac34dcb58a32f37b32
Oleg <swigerd2l@gmail.com>
2024-04-02 16:10:53 +0300

    init prisma

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(process.env.SERVER_GLOBAL_PREFIX);
+
   await app.listen(3000);
 }
+
 bootstrap();
