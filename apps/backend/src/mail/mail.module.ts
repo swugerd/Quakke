91a203453da287f1c7c269d20df48c25073c5528
Oleg <swigerd2l@gmail.com>
2024-05-07 17:31:05 +0300

    email sending for prod fix

--- apps/backend/src/mail/mail.module.ts
+++ apps/backend/src/mail/mail.module.ts
@@ -9,28 +9,37 @@ import { MailService } from './mail.service';
 @Module({
   imports: [
     MailerModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        transport: {
-          host: configService.get(config.SMTP_HOST),
-          port: configService.get(config.SMTP_PORT),
-          secure:
-            configService.get(config.SMTP_SECURE) === 'false' ? false : true,
-          // auth: {
-          //   user: configService.get(config.SMTP_USER),
-          //   pass: configService.get(config.SMTP_PASSWORD),
-          // },
-        },
-        defaults: {
-          from: `"No Reply" <${configService.get(config.SMTP_FROM)}>`,
-        },
-        template: {
-          dir: join(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
+      useFactory: async (configService: ConfigService) => {
+        const auth =
+          configService.get(config.NODE_ENV) === 'production'
+            ? {
+                auth: {
+                  user: configService.get(config.SMTP_USERNAME),
+                  pass: configService.get(config.SMTP_USER_PASSWORD),
+                },
+              }
+            : {};
+
+        return {
+          transport: {
+            host: configService.get(config.SMTP_HOST),
+            port: configService.get(config.SMTP_PORT),
+            secure:
+              configService.get(config.SMTP_SECURE) === 'false' ? false : true,
+            ...auth,
           },
-        },
-      }),
+          defaults: {
+            from: `"No Reply" <${configService.get(config.SMTP_FROM)}>`,
+          },
+          template: {
+            dir: join(__dirname, 'templates'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
       inject: [ConfigService],
     }),
   ],
